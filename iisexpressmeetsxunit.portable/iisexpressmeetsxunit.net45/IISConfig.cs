a8e4c0b3bb960b8ebff21ac007eef936482a1270
Alexander Bleissem <alexander@bleissem.com>
2017-07-18 02:00:29 +0200

    add .net 4.5 dependency

---
+++ iisexpressmeetsxunit.portable/iisexpressmeetsxunit.net45/IISConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iisexpressmeetsxunit.net45
+{
+    public class IISConfig
+    {
+
+        public FileInfo GetIISExpress()
+        {
+            string iisExpress = @"IIS Express\iisexpress.exe";
+
+            if (Environment.Is64BitOperatingSystem)
+            {
+                return new System.IO.FileInfo(Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonProgramFiles), iisExpress));
+
+            }
+            else
+            {
+                return new System.IO.FileInfo(Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonProgramFilesX86), iisExpress));
+            }            
+        }
+    }
+}
